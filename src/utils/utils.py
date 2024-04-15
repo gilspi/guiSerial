792396de83d7f2e527f57457ffecd2ba3ae4c774
gilspi <gilspi@email.con>
2024-04-15 17:24:04 +0300

    added new key/value to know type of button

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,42 +2,106 @@ import re
 import json
 
 
-def parse_file(path: str):
-    data = {}
+def search_data(string: str) -> dict:
+    string = re.sub(r'\. #\d+ bit(?:s)?', '', string)  # remove substring .D bit/bits
+
+    pattern = re.compile(r"(Lock detect|Detected value|force|val)")
+    cleaned_text = pattern.sub("", string)
+    arr_strings = cleaned_text.split(', ')
+
+    data = []
+    for string_ in arr_strings:
+        if "Reserved" in string_:
+            continue
+        data.append(string_.strip().split(' - '))
+
+    result = {}
+
+    for item in data:
+        key = item[1] if len(item) >= 2 else "INT"
+        value = item[0].strip()
+
+        if '-' in value:
+            min_val, max_val = map(int, value.split('-'))
+            value = {"min": min_val, "max": max_val}
+        elif 'MHz' in key:
+            max_val, units = key.split(" ")
+            min_val, max_value = int(value), int(max_val)
+            value = {"min": min_val, "max": max_val, "units": units}
+            key = "frequency"
+        else:
+            value = int(value)
+
+        result[key] = value
+
+    if 'Off/On' in result:
+        result['State'] = result.pop('Off/On')
+
+    return result
+
+
+def parse_file(path: str) -> dict:
+    d = {}
 
     with open(path, 'r') as file:
         content = file.read()
 
         sections = re.findall(r"## (.+?)\n(.+?)(?=\n\n## |\Z)", content, re.DOTALL)
         for section_name, section_content in sections:
-            parameters = re.findall(r"\d+\. (.+?): (.+?)\n", section_content)
+
+            parameters = re.findall(r"(\d+\. .+?): (.+?)\n|Byte\s+(\d+)", section_content)
             section_data = []
 
-            for param_name, description in parameters:
+            current_value, byte_, type_ = 0, None, None
+            offset_bits, size = 0, 0
+
+            for param_name, description, bytes_ in parameters:
+                param_name = re.sub(r'\d+\.', '', param_name).strip()
                 match = re.search(r'(\d+) bits?', description)
 
-                param_info = {
-                    "name": param_name,
-                    "id": param_name.lower().replace(" ", "_"),
-                    "size": int(match.group(1)) if match is not None else None,
-                    "offset_bytes": None,  # Пока не указано
-                    "offset_bits": None,  # Пока не указано
-                    "params": []
-                }
-                bit_values = re.findall(r'(\d+) - (.+?)\b', description)
-                for bit_value, bit_description in bit_values:
-                    param_info["params"].append({bit_value: bit_description})
+                if bytes_ != "":
+                    prev = current_value
+                    current_value = int(bytes_)
 
-                section_data.append(param_info)
+                    if prev != current_value:
+                        offset_bits = 0
 
-            data[section_name.lower().replace(" ", "_")] = section_data
+                if bytes_ == "":
+                    byte_ = current_value
 
-    return data
+                    size = int(match.group(1)) if match is not None else 8
+                    if size == 8 and byte_ in range(4, 13, 2) and param_name not in ['CMP_TH_LOW', 'VDET_RX_MAXPWR', 'ATT_IN_COM']:
+                        offset_bits = 16
+
+                    if size == 1:
+                        type_ = 'checkbutton'
+                    elif size in range(2, 5):
+                        type_ = 'radiobutton'
+                    else:
+                        type_ = 'text'
+
+                    param_info = {
+                        "name": param_name,
+                        "id": param_name.lower().replace(" ", "_"),
+                        "size": size,
+                        "offset_bytes": byte_,
+                        "offset_bits": offset_bits,
+                        "type": type_,
+                        "params": [],
+                    }
+
+                    offset_bits += size
+                    found_data = search_data(description)
+
+                    param_info["params"] = found_data
+                    section_data.append(param_info)
+
+                d[section_name.lower().replace(" ", "_")] = section_data
+    return d
 
 
 FILE_PATH = "../../ASK_IF_PROTO.MD"
 parse_data = parse_file(FILE_PATH)
-print(parse_data)
 
 JSON_FILE_PATH = "../parsed_data.json"
 with open(JSON_FILE_PATH, "w") as data:
